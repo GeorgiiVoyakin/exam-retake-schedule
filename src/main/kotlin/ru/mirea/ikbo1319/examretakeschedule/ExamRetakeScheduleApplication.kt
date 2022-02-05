9733c43baaae4159177f4f4722dceccab9ca9523
Георгий Воякин <voyaking@gmail.com>
2022-02-05 20:11:29 +0300

    Basic project

---
+++ src/main/kotlin/ru/mirea/ikbo1319/examretakeschedule/ExamRetakeScheduleApplication.kt
@@ -0,0 +1,11 @@
+package ru.mirea.ikbo1319.examretakeschedule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExamRetakeScheduleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExamRetakeScheduleApplication>(*args)
+}
---
+++ src/test/kotlin/ru/mirea/ikbo1319/examretakeschedule/ExamRetakeScheduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.examretakeschedule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExamRetakeScheduleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
