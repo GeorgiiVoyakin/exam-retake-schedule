3a8fe1ddf52c97b880a53775e745f9745e481644
Георгий Воякин <voyaking@gmail.com>
2022-03-20 00:09:43 +0300

    Updated RetakeInfoDTO values

--- src/main/kotlin/ru/mirea/ikbo1319/dto/RetakeInfoDTO.kt
+++ src/main/kotlin/ru/mirea/ikbo1319/dto/RetakeInfoDTO.kt
@@ -1,33 +1,26 @@
 package ru.mirea.ikbo1319.dto
 
 import ru.mirea.ikbo1319.data.RetakeInfo
-import java.time.LocalDate
-import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 data class RetakeInfoDTO(
-    var date: LocalDate,
-    var time: LocalTime,
+    var date: String,
+    var time: String,
     var place: String,
-    var note: String,
+    var note: String?,
     var teacher: String,
     var group: List<String>,
     var subject: String,
+    var department: String,
 ) {
     constructor(retakeInfo: RetakeInfo) : this(
-        retakeInfo.date,
-        retakeInfo.time,
+        retakeInfo.date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+        retakeInfo.time.format(DateTimeFormatter.ofPattern("hh:mm")),
         retakeInfo.place,
         retakeInfo.note,
         retakeInfo.teacher.name,
         listOf(retakeInfo.group.name),
         retakeInfo.subject.name,
+        retakeInfo.subject.department.name,
     )
-
-    override fun equals(other: Any?) = (other is RetakeInfoDTO)
-            && date == other.date
-            && time == other.time
-            && place == other.place
-            && note == other.note
-            && teacher == other.teacher
-            && subject == other.subject
 }
